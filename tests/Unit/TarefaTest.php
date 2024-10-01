baeab4e136fb3321ba805705de2a227ca2517ad7
gabigpalharini <gabigplharini@gmail.com>
2024-10-01 10:27:35 -0300

    teste

---
+++ tests/Unit/TarefaTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Tarefa;
+use Tests\TestCase;
+
+class TarefaTest extends TestCase
+{
+    public function test_criar_tarefa_de_teste()
+    {
+        $tarefa = Tarefa::create([
+            'titulo' => 'Tarefa de Teste',
+            'descricao' => 'Criando uma tarefa de Teste',
+            'concluida ' => false
+    
+        ]);
+        $this->assertEquals('Tarefa de Teste', $tarefa->titulo);
+        $this->assertEquals('Criando uma tarefa de Teste', $tarefa->descricao);
+            $this->assertFalse($tarefa->concluida);
+    }
+}
